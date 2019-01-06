86a433f82c2a37f306d5d0f5f6d2db43d88ef90e
Pierce Lauring <pclauring@gmail.com>
2019-01-05 19:54:04 -0500

    added pokemon search by name & retrieve pokemon sprites

--- src/components/App/component.js
+++ src/components/App/component.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from '../Home';
-import Form from '../Form';
+import Form from '../PokemonGenerator';
+import PokemonGenerator from '../PokemonGenerator';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Form />
+        <PokemonGenerator />
         <Home />
       </div>
     );
---
+++ src/components/PokemonGenerator/component.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+
+class Form extends Component {
+  state = { userName: '' }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    //console.log("Event: Submit ", this.state.userName);
+    // axios.get(`https://api.github.com/users/${this.state.userName}`)
+
+    // axios.get(` https://pokeapi.co/api/v2/pokemon/ditto`, {
+    //   headers: { 
+    //   'Access-Control-Allow-Origin' : '*',
+    //   },
+    // responseType: 'json',
+    //  })
+    axios.get(` https://pokeapi.co/api/v2/pokemon/${this.state.userName}`)
+      .then(resp => {
+        console.log(resp);
+        this.props.onSubmit(resp.data);
+      });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Input type="text"
+          onChange={(event) => this.setState({ userName: event.target.value })}
+          placeholder="Pokemon Name" />
+        <Button type="submit">Add Pokemon</Button>
+      </form>
+    )
+  };
+};
+
+const Card = (props) => {
+	return (
+  	<div>
+  	  <img width ="75" src={props.sprites.front_default}/>
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+        <div  style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
+      <div>{props.weight}</div>
+      </div>
+  	</div>
+  );
+};
+
+const CardList = (props) => {
+  return (
+  	<div>
+			{props.cards.map(card => <Card {...card} />)}
+  	</div>
+  );
+};
+
+class PokemonGenerator extends React.Component {
+  state = { cards: [] };
+
+  addNewCard = (cardInfo) => {
+    this.setState(prevState => ({
+      cards: prevState.cards.concat(cardInfo)
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.addNewCard} />
+        <CardList cards={this.state.cards} />
+      </div>
+    )
+  };
+};
+export default PokemonGenerator;
